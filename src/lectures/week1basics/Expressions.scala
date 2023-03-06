0d4c11046683e1f0387758f726f9edd203371577
Igarik <igarik10@yandex.ru>
2023-03-06 16:18:55 +0300

    2

--- src/lectures/week1basics/Expressions.scala
+++ src/lectures/week1basics/Expressions.scala
@@ -8,4 +8,10 @@ object Expressions extends App {
 
 //  val aCondition = true
   println(if(aCondition) "True Condition" else "False Condition") // выведет True Condition
+
+  val aNumber = if (("string".length == 6 & 1 < 2) & ('1' +: "23" :+ '4').toInt == 1234) 24 else 123
+  println(aNumber)
+
+  val someVal: Unit = print("It is just a value")
+  print(someVal)
 }
---
+++ src/lectures/week1basics/Recursion.scala
@@ -0,0 +1,11 @@
+package lectures.week1basics
+
+object Recursion extends App {
+
+
+  var i = 0
+  val whileVal: Unit = while (i < 3) {
+    println("hello")
+    i += 1
+  }
+}
